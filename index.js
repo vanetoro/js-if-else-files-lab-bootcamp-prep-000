d4344dec8f3b647335975eeca67b18ef204eca04
Vanessa Toro <vanetoro84@gmail.com>
2018-01-13 02:34:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var name = "Susan"
-
-if(name === 'Susan'){
-  console.log('The name is correct')
-} else{
-  console.log('Expected the name to be Susan')
-}
+var name = "Joe"
 var height = "74"
 
 
